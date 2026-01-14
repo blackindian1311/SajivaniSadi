b67fad59eb22898c17438470bf97132e2ddfd089
blackindian1311 <velhalatty1311@gmail.com>
2026-01-14 19:34:10 +1300

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,17 @@
-// firebase.js
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKNy8hWMuJZPPdVO2o1Jlmz_FMg4Z7wIE",
-  authDomain: "moneytracker-bc2a1.firebaseapp.com",
-  projectId: "moneytracker-bc2a1",
-  storageBucket: "moneytracker-bc2a1.appspot.com",
-  messagingSenderId: "748578310449",
-  appId: "1:748578310449:web:6f60250d03f032856c810",
-  measurementId: "G-6HJWLTP7HZ"
+  apiKey: "AIzaSyCWtEEJJpoPTJtBU9ECpa1Ps2IOJyHZLSY",
+  authDomain: "sanjiwanisari-2222e.firebaseapp.com",
+  projectId: "sanjiwanisari-2222e",
+  storageBucket: "sanjiwanisari-2222e.firebasestorage.app",
+  messagingSenderId: "773050300354",
+  appId: "1:773050300354:web:1e0133afa9fd6f2cf8885b"
 };
 
-
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-export { db, auth };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
